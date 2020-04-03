4b8eaf664dc8b0d13a49f087e6cbbc8c61a5b835
varshlav <46642344+varshlav@users.noreply.github.com>
2020-04-03 14:12:59 -0500

    Css changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <div>
-      <input id="add" v-model="task"> <button @click="addTodo">Add Task </button>
+    <header class="header">
+      <h1>TodoList</h1>
+    </header>
+    <div class="todocontent">
+      <div class="addtodo">
+        <input type="text" id="add" v-model="task"/> 
+        <button type="submit" @click="addTodo" class="btn">Add Task </button>
+      </div>
+        
+      <h3> Following is the List - </h3>
+      <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
     </div>
-   Following is the List - 
-    
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
@@ -19,6 +25,7 @@ export default {
   },
   data(){
     return {
+      task: '',
       todos: [
         {id: 1,
         title: 'Buy Groceries',
@@ -32,7 +39,7 @@ export default {
         title: 'Complete Assignment',
         completed: false
         }],
-        total : 3
+        total : 4
     }
   },
   methods: {
@@ -52,12 +59,64 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  box-sizing :border-box;
+  margin:0;
+  padding:0;
 }
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height:1.4;
+}
+#app{
+  width:800px;
+  margin: 10px auto;
+  border: 1px solid #000;
+}
+.header{
+  background:#333;
+  color:#fff;
+  text-align:center;
+  padding:10px;
+}
+.header a{
+  color:#fff;
+  padding-right:5px;
+  }
+
+   .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+    border: 1px solid #555;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px
+  }
+  .btn:hover {
+    background: #666;
+  }
+ 
+  .addtodo {
+    display: flex;
+     padding:10px;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+    border-top-left-radius: 5px;
+    border: 1px solid #737171;
+    border-bottom-left-radius: 5px;
+    font-size:14px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+  .todocontent h3{
+   padding:10px;
+
+  }
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,9 @@
 <template>
-    <div v-bind:class="{'is-complete':todo.completed}">
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         
-        <p><input type="checkbox" v-on:change="changeStatus"> {{todo.title}}
-            <button @click="$emit('delete',todo.id)"> X </button>
+        <p>
+            <input class="checkbox" type="checkbox" v-on:change="changeStatus"> {{todo.title}}
+            <button @click="$emit('delete',todo.id)" class="del"> X </button>
         </p>
     </div>
 </template>
@@ -20,7 +21,24 @@ export default {
 }
 </script>
 <style scoped>
-.is-complete{
+.todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
     text-decoration: line-through;
-}
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+  .checkbox{
+      margin-right:5px;
+  }
 </style>
