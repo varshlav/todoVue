63d2a49160c107885b7bba66ad3e5923d0c7c1eb
varshlav <46642344+varshlav@users.noreply.github.com>
2020-04-03 11:17:46 -0500

    Uploading project

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div>
+      <input id="add" v-model="task"> <button @click="addTodo">Add Task </button>
+    </div>
+   Following is the List - 
+    
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {id: 1,
+        title: 'Buy Groceries',
+        completed: false
+        },
+        {id: 2,
+        title: 'Wash Clothes',
+        completed: true
+        },
+        {id: 3,
+        title: 'Complete Assignment',
+        completed: false
+        }],
+        total : 3
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id); 
+    },
+    addTodo(){
+      this.todos.push({
+      id: this.total++,
+      title: this.task,
+      completed: false
+    });
+    this.task = "";
+  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-bind:class="{'is-complete':todo.completed}">
+        
+        <p><input type="checkbox" v-on:change="changeStatus"> {{todo.title}}
+            <button @click="$emit('delete',todo.id)"> X </button>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TodoItem',
+    props: ["todo"],
+    methods: {
+        changeStatus(){
+            this.todo.completed = !this.todo.completed ;
+        }
+    }
+
+    
+}
+</script>
+<style scoped>
+.is-complete{
+    text-decoration: line-through;
+}
+</style>
